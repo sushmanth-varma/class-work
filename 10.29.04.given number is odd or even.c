de5b0c79afbd5e47ada5243ac29e201aad87f49f
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-04-29 16:19:28 +0530

    Create 10.29.04.given number is odd or even.c

---
+++ 10.29.04.given number is odd or even.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int num;
+    
+    printf("Enter the number: ");
+    scanf("%d", &num);
+    
+    if (num % 2 == 0) {
+        printf("The given number is even\n");
+    } else {
+        printf("The given number is odd\n");
+    }
+    
+    return 0;
+}
