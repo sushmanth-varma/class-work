fc2e6c9ec708893b6fa19e85346b87fd64043635
sushmanth-varma <128605935+sushmanth-varma@users.noreply.github.com>
2023-05-06 16:23:16 +0530

    Create 5)6.05 hallow diamond.c

---
+++ 5)6.05 hallow diamond.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+int main()
+{
+ 
+    int n = 5, rows, columns;
+    for (rows = 1; rows <= n; rows++) {
+        for (columns = n; columns > rows; columns--) {
+            printf(" ");
+        }
+        printf("*");
+
+        for (columns = 1; columns < (rows - 1) * 2;
+             columns++) {
+            printf(" ");
+        }
+        if (rows == 1) {
+            printf("\n");
+        }
+        else {
+            printf("*\n");
+        }
+    }
+    for (rows = n - 1; rows >= 1; rows--) {
+ 
+        // used for printing the spaces
+        for (columns = n; columns > rows; columns--) {
+            printf(" ");
+        }
+        printf("*");
+        for (columns = 1; columns < (rows - 1) * 2;
+             columns++) {
+            printf(" ");
+        }
+        if (rows == 1) {
+            printf("\n");
+        }
+        else {
+            printf("*\n");
+        }
+    }
+    return 0;
+}
